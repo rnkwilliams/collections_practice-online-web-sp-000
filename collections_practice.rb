1ab9c7f2151c1a51193c43007b007d61cac394d9
Ranika Williams <rnkprice07@gmail.com>
2019-05-14 16:14:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -43,4 +43,12 @@ end
    numbers.inject{|sum, n| sum + n}
  end
 
- 
+ def add_s(array)
+  array.each_with_index.collect do |string, index|
+    if index == 1
+      string
+    else
+      string << "s"
+    end
+  end
+end
